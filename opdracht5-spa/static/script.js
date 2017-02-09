163bf7d73cf0602477369142a6efa0df7b9ca933
dylanvans <dylanvansprang@gmail.com>
2017-02-09 11:40:24 +0100

    added if statement with hashchange

--- opdracht5-spa/static/script.js
+++ opdracht5-spa/static/script.js
@@ -28,16 +28,20 @@ var View = function() {
 
 View.prototype.toggle = function() {
 	this.hideClass = 'js-hide';
-	this.activeViewEl = document.querySelector(window.location.hash);
-	this.viewEl = document.getElementsByClassName('view');
 
-	for (var i = 0; i < this.viewEl.length; i++) {
-		if(this.activeViewEl.id == this.viewEl[i].id) {
-			this.viewEl[i].classList.remove(this.hideClass);
-		} else {
-			this.viewEl[i].classList.add(this.hideClass);
+	if(window.location.hash) {
+		this.activeViewEl = document.querySelector(window.location.hash);
+		this.viewEl = document.getElementsByClassName('view');
+
+		for (var i = 0; i < this.viewEl.length; i++) {
+			if(this.activeViewEl.id == this.viewEl[i].id) {
+				this.viewEl[i].classList.remove(this.hideClass);
+			} else {
+				this.viewEl[i].classList.add(this.hideClass);
+			}
 		}
 	}
+	
 }
 
 new App();
